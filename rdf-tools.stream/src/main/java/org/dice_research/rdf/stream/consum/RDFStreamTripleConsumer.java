37b855dfd560fbe337003be7511dda192ca5c278
Michael Röder <michael.roeder@uni-paderborn.de>
2023-04-21 18:36:55 +0200

    Added a simple class that allows to add a consumer to an RDF stream.

---
+++ rdf-tools.stream/src/main/java/org/dice_research/rdf/stream/consum/RDFStreamTripleConsumer.java
@@ -0,0 +1,34 @@
+package org.dice_research.rdf.stream.consum;
+
+import java.util.function.Consumer;
+
+import org.apache.jena.graph.Triple;
+import org.apache.jena.riot.system.StreamRDFBase;
+import org.apache.jena.sparql.core.Quad;
+
+/**
+ * This class can be used to let the given {@link Consumer} consume triples from
+ * an RDF stream.
+ * 
+ * @author Michael R&ouml;der (michael.roeder@uni-paderborn.de)
+ *
+ */
+public class RDFStreamTripleConsumer extends StreamRDFBase {
+
+    private Consumer<Triple> consumer;
+
+    public RDFStreamTripleConsumer(Consumer<Triple> consumer) {
+        super();
+        this.consumer = consumer;
+    }
+
+    @Override
+    public void triple(Triple triple) {
+        consumer.accept(triple);
+    }
+
+    @Override
+    public void quad(Quad quad) {
+        triple(quad.asTriple());
+    }
+}
