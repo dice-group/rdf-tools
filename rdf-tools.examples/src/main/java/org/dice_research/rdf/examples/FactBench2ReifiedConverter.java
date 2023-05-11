d631cf367a1416ad22cc13e076fafe00c0f3c785
Michael Röder <michael.roeder@uni-paderborn.de>
2023-05-11 17:14:12 +0200

    Added a class to concatenate compressed RDF files.

--- rdf-tools.examples/src/main/java/org/dice_research/rdf/examples/FactBench2ReifiedConverter.java
+++ rdf-tools.examples/src/main/java/org/dice_research/rdf/examples/FactBench2ReifiedConverter.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 
 import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.graph.Node;
@@ -35,56 +37,79 @@ public class FactBench2ReifiedConverter {
     public static void main(String[] args) throws IOException {
         if (args.length < 4) {
             System.err.println(
-                    "Error: wrong usage. FactBench2ReifiedConverter <true-directory> <false-directory> <dataset-namespace> <output-file>");
+                    "Error: wrong usage. FactBench2ReifiedConverter <true-directory> <false-directory> <dataset-namespace> <output-file> [use-count]");
             return;
         }
         File trueDirectory = new File(args[0]);
         File falseDirectory = new File(args[1]);
-        String namespace = args[2];
-        if (!namespace.endsWith("/") && !namespace.endsWith("#")) {
-            namespace += "/";
+        String tempNS = args[2];
+        if (!tempNS.endsWith("/") && !tempNS.endsWith("#")) {
+            tempNS += "/";
         }
+        final String namespace = tempNS;
         String outputFile = args[3];
+
+        final AtomicInteger iriCount = new AtomicInteger(0);
+        Function<String, String> fileName2StmtIri;
+        // boolean useCount4StmtIri = false;
+        if ((args.length > 4) && (Boolean.parseBoolean(args[4]))) {
+            fileName2StmtIri = s -> namespace + iriCount.getAndIncrement();
+        } else {
+            fileName2StmtIri = s -> namespace + s;
+        }
 
         // Recursively go through the directory and parse the files
         int count = 0;
         try (Writer writer = new FileWriter(outputFile)) {
             StreamRDF stream = StreamRDFLib.writer(writer);
             stream.start();
-            count = readDirectory(trueDirectory, namespace, TRUE_VALUE, stream);
-            count += readDirectory(falseDirectory, namespace, FALSE_VALUE, stream);
+            count = readDirectory(trueDirectory, fileName2StmtIri, TRUE_VALUE, stream);
+            count += readDirectory(falseDirectory, fileName2StmtIri, FALSE_VALUE, stream);
             stream.finish();
         }
         LOGGER.info("Finished. Processed {} files.", count);
     }
 
-    private static int readDirectory(File inputDirectory, String namespace, Node veracityValue, StreamRDF stream) {
+    private static int readDirectory(File inputDirectory, Function<String, String> fileName2StmtIri, Node veracityValue,
+            StreamRDF stream) {
         LOGGER.info("Processing files in {} ...", inputDirectory);
         int count = 0;
-        Resource event;
-        Resource[] triple;
         for (File inputFile : inputDirectory.listFiles()) {
             if (inputFile.isDirectory()) {
                 // read directory
-                count += readDirectory(inputFile, namespace, veracityValue, stream);
+                count += readDirectory(inputFile, fileName2StmtIri, veracityValue, stream);
             } else {
                 // read file
                 Model model = ModelFactory.createDefaultModel();
+                Resource[] triple;
                 try {
                     model.read(inputFile.toURI().toURL().toString());
-                    event = selectEvent(model);
-                    if (event != null) {
-                        triple = selectTriple(event, model);
-                        if (triple != null) {
-                            writeReifiedStmt(triple, namespace, veracityValue, inputFile, stream);
-                            ++count;
-                        } else {
-                            LOGGER.warn(
-                                    "Couldn't find triple for intermediate node {} in {}. The file will be ignored.",
-                                    event.toString(), inputFile);
-                        }
+                    if (model.size() == 1) {
+                        // This file is a special case in which only the triple that should be checked
+                        // is within the file
+                        StmtIterator iterator = model.listStatements();
+                        Statement s = iterator.next();
+                        writeReifiedStmt(
+                                new Resource[] { s.getSubject(), s.getPredicate(), s.getObject().asResource() },
+                                fileName2StmtIri.apply(inputFile.getName()), veracityValue, stream);
+                        ++count;
                     } else {
-                        LOGGER.warn("Couldn't find intermediate node in {}. The file will be ignored.", inputFile);
+                        Resource event;
+                        event = selectEvent(model);
+                        if (event != null) {
+                            triple = selectTriple(event, model);
+                            if (triple != null) {
+                                writeReifiedStmt(triple, fileName2StmtIri.apply(inputFile.getName()), veracityValue,
+                                        stream);
+                                ++count;
+                            } else {
+                                LOGGER.warn(
+                                        "Couldn't find triple for intermediate node {} in {}. The file will be ignored.",
+                                        event.toString(), inputFile);
+                            }
+                        } else {
+                            LOGGER.warn("Couldn't find intermediate node in {}. The file will be ignored.", inputFile);
+                        }
                     }
                     model.close();
                 } catch (Exception e) {
@@ -130,9 +155,8 @@ public class FactBench2ReifiedConverter {
         return null;
     }
 
-    private static void writeReifiedStmt(Resource[] triple, String namespace, Node veracityValue, File inputFile,
-            StreamRDF stream) {
-        Node stmt = NodeFactory.createURI(namespace + inputFile.getName());
+    private static void writeReifiedStmt(Resource[] triple, String stmtIri, Node veracityValue, StreamRDF stream) {
+        Node stmt = NodeFactory.createURI(stmtIri);
         // Write triple
         StreamUtils.writeReifiedStatement(stmt, triple[0].asNode(), triple[1].asNode(), triple[2].asNode(), stream);
         // Add veracity value
